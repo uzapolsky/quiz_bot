0f50515aaa771ff929504f05880989a776095d70
uzapolsky <uzapolsky@gmail.com>
2021-11-17 18:07:01 +0300

    add questions and parser

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+import random
+
+
+def main():
+    quiz_questions = {}
+    text_sections = []
+    for filename in os.listdir('quiz-questions'):
+        with open(f'quiz-questions/{filename}', encoding='KOI8-R') as f:
+            text_sections.extend(f.read().split('\n\n'))
+        
+    for index, current_section in enumerate(text_sections):
+        if (current_section.startswith('Вопрос') and
+                index+1 < len(text_sections) and
+                text_sections[index+1].startswith('Ответ')):
+            key = current_section.split(':', 1)[1].strip()
+            quiz_questions[key] = text_sections[index+1].split(':', 1)[1].strip()
+    
+    country, capital = random.choice(list(quiz_questions.items()))
+    print(country , '\n-----\n', capital)
+
+if __name__ == '__main__':
+    main()
