bc2af4e21c04edd8716ca0bc3cc6efb660a43c80
uzapolsky <uzapolsky@gmail.com>
2021-11-18 12:46:44 +0300

    add tg echo bot

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import os
 import random
 
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from dotenv import load_dotenv
 
-def main():
+def parse_questions():
     quiz_questions = {}
     text_sections = []
     for filename in os.listdir('quiz-questions'):
@@ -17,6 +19,23 @@ def main():
             quiz_questions[key] = text_sections[index+1].split(':', 1)[1].strip()
     
     question, answer = random.choice(list(quiz_questions.items()))
+
+
+def echo(bot, update):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def main():
+    load_dotenv()
+    updater = Updater(os.environ['TG_BOT_TOKEN'])
+
+    dp = updater.dispatcher
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
