438322bde4924cb226218fc815a67f791b1c3e7a
uzapolsky <uzapolsky@gmail.com>
2021-11-23 15:14:52 +0300

    add all for checking conditions in parse_questions.py

--- parse_questions.py
+++ parse_questions.py
@@ -9,9 +9,11 @@ def parse_questions(questions_folder):
             text_sections.extend(f.read().split('\n\n'))
         
     for index, current_section in enumerate(text_sections):
-        if (current_section.startswith('Вопрос') and
-                index+1 < len(text_sections) and
-                text_sections[index+1].startswith('Ответ')):
+        if all([
+            current_section.startswith('Вопрос'),
+            index+1 < len(text_sections),
+            text_sections[index+1].startswith('Ответ')
+        ]):
             question = current_section.split(':', 1)[1].strip()
             quiz_questions[question] = text_sections[index+1].split(':', 1)[1].strip()
     
